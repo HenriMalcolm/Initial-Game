e201334c60d5f1211693309e62894329367abf9d
HenriMalcolm <129811931+HenriMalcolm@users.noreply.github.com>
2023-04-06 10:11:47 +1200

    Player movement

---
+++ Initial-Game-Unity 2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {   
+        //Moves player left, right, up and down
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
